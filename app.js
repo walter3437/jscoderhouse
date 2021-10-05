c653118d3603a17b884edb9eaa9c2ed575b4eada
walter luis seco <walterseco2293@gmail.com>
2021-10-05 09:35:41 -0300

    primer desafio js i#

---
+++ app.js
@@ -0,0 +1,12 @@
+//Ingreso de Datos
+
+let pesoMedia = parseFloat (prompt ("Peso MediaRes:"))
+
+//Cortes
+
+const entraña = .004
+
+let rinde=( "Entraña:" + " " + entraña * pesoMedia + " " + "Kg.")
+
+console.log(rinde)
+alert(rinde)
