0db60870ee4acdcce851e194cfde948bdb232718
walter luis seco <walterseco2293@gmail.com>
2021-10-07 13:25:00 -0300

    segundo desafio coderhouse

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
-//Ingreso de Datos
-let nombre = prompt ("Ingrese su nombre")
 
-let saludo =("Bienvenido" + " " + nombre + "!")
-alert(saludo)
+const a = .25
+const b = .50
+const c = .75
 
-let pesoMedia = parseFloat (prompt ("Peso MediaRes:"))
+let nombre = prompt("Ingresá tu nombre")
 
-//Cortes
+if (nombre = Number) {
 
-const entraña = .004
+    alert ("El valor ingresado no es valido")
+} else {
 
-let rinde=( "Entraña:" + " " + entraña * pesoMedia + " " + "Kg.")
+    let kilogramos = parseInt(prompt("Ingrese los Kg."))
+    
+    if (isNaN(kilogramos)) {
 
-console.log(rinde)
-alert(rinde)
+        alert ("El valor ingresado es invalido")
+    } else if (kilogramos <= 0) {
+        alert("El numero ingresado debe ser mayor a 0")
+    } else {
+        let corte = prompt(nombre + " " + "Ingresá \nA) Entraña \nB) Matambre \nC) Costillas")
+    
+        if (corte == "a") {
+            alert("Entraña:" + " " + a * kilogramos + " " + "Kg.")
+        } else if (corte == "b") {
+            alert("Matambre:" + " " + b * kilogramos + " " + "Kg.")
+        } else if (corte == "c") {
+            alert("Costilla:" + " " + c * kilogramos + " " + "Kg.")
+        } else {
+            alert ("La opción ingresada no es válida")
+        }
+    }
+}
